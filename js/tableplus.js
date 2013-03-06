e75455c55b3ea867be8b86f869cb82536f8cf832
ribbon10 <ribbon10@server>
2013-03-06 08:30:13 +0100

    initial commit at the moment it is more like a draft that is running untested on chrome.

---
+++ js/tableplus.js
@@ -0,0 +1,239 @@
+(function( $ ) {
+  $.fn.tableplus = function() {
+
+  	var methods = {
+      scrollbar : function( table ){
+        // create elements
+        var scrollbar_container = $('<div class="table_scollbar_container"></div>'),
+            scrollbar_slider = $('<div class="table_scrollbar_slider"></div>');
+        // compose scollbar elements
+        scrollbar_container.append(scrollbar_slider);
+        table.after(scrollbar_container);
+
+        var update_table_scrollbars = function(){
+          // get relevant values
+          var window_top = window.scrollY,
+              window_height = $(window).height(),
+              table_body = table.find('> tbody'),
+              table_body_top = table_body.position().top,
+              table_body_bottom = table_body_top+table_body.height();
+          // calc begin and end of table body
+          var begin = (window_top < table_body_top) ? table_body_top : window_top;
+          var end = (window_top+window_height < table_body_bottom) ? window_top+window_height : table_body_bottom;
+          // check if we see the table body
+          if( begin > end ) { // we do not see the table body
+            // hide scrollbar if table body is not seen
+            scrollbar_container.css({visibility: 'hidden'});
+          }
+          else { // we see the table body
+            // show scrollbar if table body is seen
+            scrollbar_container.css({visibility: 'visible'});
+            // get relevant values
+            var table_header = table.find('thead'),
+                table_header_height = table_header.height(),
+                table_body_height = table_body.outerHeight();
+            // do fixed table header correction only if fixed header class is applied to table
+            var correction_for_fixed_header = 0;
+            if( table.hasClass('table-sticky-header') )
+              correction_for_fixed_header = (begin-window_top > table_header_height) ? 0 : table_header_height-(begin-window_top);
+            // change css of container and slider
+            var height = end-begin-correction_for_fixed_header;
+            scrollbar_container
+              .css( { height: height,
+                      top: begin+correction_for_fixed_header, 
+                      left: table_body.position().left+table.outerWidth() } );
+            scrollbar_slider
+               .css( { top: (begin-table_body_top+correction_for_fixed_header)/(table_body_height)*(height), 
+                       height: (end-begin-correction_for_fixed_header)/(table_body_height)*(height) } );
+          }
+        }
+
+        // update initialy
+        update_table_scrollbars();
+        // hook scrolling
+        $(window).scroll( function(eventObject){
+          update_table_scrollbars();
+        });
+        // hook resize
+        $(window).resize( function(eventObject){
+          update_table_scrollbars();
+        });
+      },
+      sticky_header : function( table ){
+        var header = table.find('thead');
+        var footer = table.find('> tfoot');
+        // select all table rows
+        var header_rows = header.find('> tr')
+        // clone all table rows
+        var header_rows_original = header_rows.clone()
+        // adapt height of rows
+        header_rows.height( function(index, height){
+          return height;
+        });
+        // set width of every column
+        header_rows.find('> th').width( function(index, width){
+          return width;
+        });
+        // insert cloned rows so that they get corrrect dynmensions
+        header.prepend( header_rows_original );
+        // add special class to rows
+        header_rows.addClass('table-sticky-header-row');
+        // special class for first sticky header row
+        header_rows.eq(0).addClass('table-sticky-header-row-first');
+        // TODO remove these old row class
+        header_rows_original.addClass('table-sticky-header-row-original');
+        // adapt margin-top of row -> this is necessary for mulitple header rows
+        var header_height_accumulated = 0;
+        header_rows.each( function(index, row){
+          row = $(row);
+          row_height = row.outerHeight();
+          $(row).css('margin-top', header_height_accumulated);
+          header_height_accumulated += row_height;
+        });
+
+        // function for updating position of header
+        var update_fixed_table_header = function(){
+          header_rows.each(function(index, header){
+            var header = $(header);
+            // read relevant values
+            var window_top = window.scrollY,
+                table_top = table.position().top,
+                table_height = table.height(),
+                header_top = header_rows_original.position().top,
+                header_height = header_height_accumulated, //header.height(),
+                footer_height = footer.height();
+            // deside weather the table is out of scroll
+            if( window_top>table_top && window_top<table_top+table_height ) {
+              //console.log('adapt header');
+              // check if the header should be fixed or if it should scrolle out
+              if( window_top<header_top+table_height-header_height-footer_height ) {
+                //console.log('fixed header -> we are insite the table');
+                header.css( { position: 'fixed',
+                              top: 0 } )
+                      .addClass('table-sticky-header-row-is-sticky');
+              }
+              else {
+                //console.log('absolute header -> we do not see the body any more and are scrolling out. perhaps with the footer');
+                header.css( { position: 'absolute',
+                              top: header_top+table_height-header_height-footer_height } )
+                      .removeClass('table-sticky-header-row-is-sticky');
+              }
+            }
+            else {
+              //console.log('normal header -> we are above the header table');
+              header.css( { position: 'absolute', 
+                            top: header_top } )
+                    .removeClass('table-sticky-header-row-is-sticky');
+            }
+          });
+        }
+
+        // update initialy
+        update_fixed_table_header();
+        // hook scrolling
+        $(window).scroll( function(eventObject){
+          update_fixed_table_header();
+        });
+        // hook resize
+        $(window).resize( function(eventObject){
+          update_fixed_table_header();
+        });
+      },
+      sortable : function( table ){
+        var columns = table.find('thead > tr > th');
+        var previous_sorted_column = -1;
+        var sort_order_asc = true;
+        columns.each( function(index, th){
+          // create link in header columns
+          var link = $('<a style="display:_block;" href="#"></a>');
+          link.append( $(th).contents() )
+              .append('<i style="opacity: 0.5;visibility:hidden;" class="icon-chevron-up pull-right table-sort-icon"></i>');
+          $(th).empty()
+            .append(link);
+          // make header responsive
+          link.click(function(eventObject){
+            // get column index that is clicked
+            var column_index = columns.index(th);
+            // check if sort order should be changed -> this happens if the same row is sorted again -> default is ascending
+            if( column_index==previous_sorted_column )
+              sort_order_asc = !sort_order_asc
+            else
+              sort_order_asc = true
+            // remember column we have sorted
+            previous_sorted_column = column_index;
+            // hide all header icons
+            columns.find('> a > i.table-sort-icon').css('visibility', 'hidden');
+            // show header icon of this column
+            columns.eq(column_index).find('> a > i.table-sort-icon').css('visibility', 'visible');
+            // set correct icon
+            if( sort_order_asc )
+              link.find('> i.table-sort-icon')
+                .removeClass('icon-chevron-down')
+                .addClass('icon-chevron-up')
+            else
+              link.find('> i.table-sort-icon')
+                .addClass('icon-chevron-down')
+                .removeClass('icon-chevron-up')
+            // get table body rows
+            var rows = $(th).closest('table').find('> tbody > tr');
+            // function for getting values of column
+            var get_value_of_column = function(row, column_index){
+              // get text of column
+              var text = $(row).find('td').eq(column_index).text();
+              // get data type of column
+              var column_data_type = columns.eq(column_index).attr('data-type');
+              // check data type of column and convert values according
+              if( 'number' == column_data_type ) {
+                text = text.replace(',', '.')*1;
+              }
+              // return test
+              return text;
+            }
+            // reverse operand for easier ascending descending sorting
+            var reverse_operand = sort_order_asc ? 1 : -1
+            // sort rows
+            rows.sort(function(a, b){
+              x = get_value_of_column(a, column_index);
+              y = get_value_of_column(b, column_index);
+              // do the sorting
+              if( x == y ) {
+                // if we are here lets iterate over all columns -> begin at the front
+                for( i = 0; i < columns.length; ++i) {
+                  x = get_value_of_column(a, i);
+                  y = get_value_of_column(b, i);
+                  if( x != y ) {
+                    return ((x<y) ? -1 : 1)*reverse_operand;
+                  } 
+                }
+                return 0;
+              }
+              else {
+                return ((x<y) ? -1 : 1)*reverse_operand;
+              }
+            });
+            // insert sorted rows
+            $(th).closest('table').find('> tbody')
+              .empty()  
+              .append(rows);
+            // do not follow link
+            return false;
+          });
+        });
+      }
+    }
+
+  	return this.each(function() {
+      var $this = $(this);
+      if( $this.hasClass('table-sortable') )
+        methods.sortable( $this );
+      if ( $this.hasClass('table-sticky-header') )
+        methods.sticky_header( $this );
+      if ( $this.hasClass('table-scollbar') )
+        methods.scrollbar( $this );
+    });
+  };
+
+  $(window).on('load', function () {
+    $('table').tableplus();
+  });
+})( jQuery );
